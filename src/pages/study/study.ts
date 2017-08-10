a7c24b63b21e18ebd904713cd5ed3206b7c69ec9
Brian Phillips <brian.phillips@qq.com>
2017-08-10 16:47:50 +0800

    tried setting up toast but failed. setup array of object with short vowels.

--- src/pages/study/study.ts
+++ src/pages/study/study.ts
@@ -15,13 +15,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StudyPage {
 
-  shortVowels = ['A', 'E', 'I', 'O', 'U'];
+  shortVowels = [{
+    'letter': 'A',
+    'word': 'Apple',
+    'img': 'https://polyphasicdevs.com/luckyenglish/img/shortVowels/apple.png'
+  },{
+    'letter': 'E',
+    'word': 'Egg',
+    'img': 'https://polyphasicdevs.com/luckyenglish/img/shortVowels/egg.png'
+  },{
+    'letter': 'I',
+    'word': 'Insect',
+    'img': 'https://polyphasicdevs.com/luckyenglish/img/shortVowels/insect.png'
+  },{
+    'letter': 'O',
+    'word': 'Orange',
+    'img': 'image'
+  },{
+    'letter': 'U',
+    'word': 'Umbrella',
+    'img': 'image'
+  }];
   longVowels = ['A', 'E', 'I', 'O', 'U'];
   consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
   blends = ['CH','SH','th','TH','oo','OO','OI','OY','ER','UR','IR','AR','OR','AW','OU','OW','ING','PH','TION'];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams  
+  ) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StudyPage');
