15476c4609998ee89c71dc71471ecc59693c3df0
Brian Phillips <brian.phillips@qq.com>
2017-08-15 14:35:09 +0800

    fixed a few cli errors: [WARN] Detected @ionic/cli-plugin-cordova in your package.json., [14:33:43]  tslint: src/app/app.module.ts, line: 5             'NavParams' is declared but never used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { ComponentsModule } from './../components/components.module';
 import { ModalComponent } from './../components/modal/modal';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler, NavParams } from 'ionic-angular';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
